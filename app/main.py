2ff78d34109ad6d701d42a0e3c1d8e5715469197
wyk <wyk@git.com>
2024-08-02 10:30:42 +0800

    update main.py 将项目根目录添加到 Python 解释器的模块搜索路径中

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from fastapi import FastAPI
 from app.models.schemas import UserCreate
 from app.api.users import users_router
