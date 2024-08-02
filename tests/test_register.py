1df1c776df97165a40ab7db2f99042366865af54
wyk <wyk@git.com>
2024-08-02 10:10:19 +0800

    update auth.py add test_register.py add requirements.txt

--- tests/test_register.py
+++ tests/test_register.py
@@ -1,17 +1,19 @@
 import requests
 
-url = "http://127.0.0.1:8080/register"
+url = "http://127.0.0.1:8080/health/user/register"
 headers = {
     'Content-Type': 'application/json'
 }
 
 data = {
-    "username": "19099933340",
-    "password": "bd123sdf6",
-    "nickname": "Candy"
+    "type": "register",
+    "username": "14510405900",
+    "password": "bd123sdf6"
 }
 
 response = requests.post(url, headers=headers, json=data)
 
 print(response.status_code)
+print(response.text)
 print(response.json())
+
